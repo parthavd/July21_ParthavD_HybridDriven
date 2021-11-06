9bb3d01693579c9b5d1dd1eb861062cbc74edcd2
Parthav Dani <partharjun@gmail.com>
2021-11-05 21:04:04 -0400

    Creating_Add_Employee_page

--- src/com/TechnoCredits/orghrm/pages/PIM_AddEmployee_PersonalDetails.java
+++ src/com/TechnoCredits/orghrm/pages/PIM_AddEmployee_PersonalDetails.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.By;
 import com.TechnoCredits.orghrm.base.PreDefinedActions;
 
 public class PIM_AddEmployee_PersonalDetails extends PreDefinedActions {
-	
+
 	public String AddEmployeeWizard_getHeaderDetails() {
-		
+
 		String header = "";
 		header = driver.findElement(By.xpath("//span[@data-tooltip='Add Employee Wizard']")).getText();
 		header = header + " " + driver.findElement(By.xpath("//span[@data-tooltip='Personal Details']")).getText();
--- src/com/TechnoCredits/orghrm/testscripts/PIM_AddEmployee_Test.java
+++ src/com/TechnoCredits/orghrm/testscripts/PIM_AddEmployee_Test.java
@@ -1,6 +1,5 @@
 package com.TechnoCredits.orghrm.testscripts;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,12 +12,11 @@ import com.TechnoCredits.orghrm.pages.PIM_AddEmployee;
 import com.TechnoCredits.orghrm.pages.PIM_AddEmployee_PersonalDetails;
 
 public class PIM_AddEmployee_Test {
-	
+
 	@BeforeClass
 	public void start() {
 		PreDefinedActions.start();
 	}
-
 
 	@Test
 	public void check_Add_Employee_Next() throws InterruptedException {
@@ -27,14 +25,15 @@ public class PIM_AddEmployee_Test {
 		MenuPage menuPage = new MenuPage();
 		menuPage.navigateTo("PIM-Add Employee");
 		PIM_AddEmployee pim_AddEmployee = new PIM_AddEmployee();
-		pim_AddEmployee.Add_EmployeeName("EmpFirstName", "EmpLastName").Add_Location("Australian Regional HQ").clickNext();
+		pim_AddEmployee.Add_EmployeeName("EmpFirstName", "EmpLastName").Add_Location("Australian Regional HQ")
+				.clickNext();
 		String expectedHeaderText = "Add Employee Wizard Personal Details";
 		PIM_AddEmployee_PersonalDetails pimAddEmpPersonalDetails = new PIM_AddEmployee_PersonalDetails();
 		String actualHeaderText = pimAddEmpPersonalDetails.AddEmployeeWizard_getHeaderDetails();
 		Assert.assertEquals(expectedHeaderText, actualHeaderText);
 	}
-	
-	
+
+	@AfterClass
 	public void close() {
 		PreDefinedActions.close();
 	}
